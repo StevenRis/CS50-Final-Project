5455e347d7882b7fa31842aa8ec35af50ad52f44
Stanisław Griszko <s.griszko@gmail.com>
2022-10-05 22:21:44 +0200

    feat: implement spinner while loading page

--- static/script.js
+++ static/script.js
@@ -1,5 +1,15 @@
-"use strict"
+// "use strict"
+function preloader(){
+    document.querySelector("#loading").style.display = "none";
+    document.querySelector("#content").style.display = "block";
+    document.querySelector("footer").style.display = "block";
 
+}//preloader
+
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+    preloader();
+})
 // Get all a tags with class .car-setup
 // It returns node list of a tags
 const setup_links = document.querySelectorAll('.car-setup');
@@ -11,6 +21,7 @@ for (let i = 0; i < setup_links.length; i++) {
     const new_url = setup_links[i].href.slice(21, -4);
     // Make change on the page
     setup_links[i].href = new_url;
-
 }
 
+
+// window.onload = preloader;
