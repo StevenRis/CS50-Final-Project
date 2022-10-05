d054047fd19061bc36a2c919f51dc87356dec25e
Stanisław Griszko <s.griszko@gmail.com>
2022-10-05 10:16:26 +0200

    feat: create layout.html, index.html, add bootstrap, create navbar

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello"
+    return render_template("index.html")
 
 
 # enable debug mode - no need to restart the server to refresh the page
