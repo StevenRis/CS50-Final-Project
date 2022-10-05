aad182e92e4d5a7b4e863555ba383158ff1a8525
Stanisław Griszko <s.griszko@gmail.com>
2022-10-05 13:40:31 +0200

    feat: create layout of location page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-import sqlite3
+import sqlite3 as sql
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def home():
 @app.route("/cars")
 def cars():
     return render_template("cars.html")
+
+
+@app.route("/locations")
+def locations():
+    """Show locations"""
+
+    connection = sql.connect("test.db")
+    connection.row_factory = sql.Row
+
+    db = connection.cursor()
+    locations = db.execute("SELECT * FROM locations")
+
+    # rows = db.fetchall()
+    return render_template("locations.html", locations=locations)
 
 
 # enable debug mode - no need to restart the server to refresh the page
