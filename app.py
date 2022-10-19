fc5ed24f8c0c35f899f0760aab1b82caadd28299
Stanisław Griszko <s.griszko@gmail.com>
2022-10-19 22:12:31 +0200

    feat: install flask session, add register and login/log out links to navbar

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, redirect, request, session, flash
+from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3 as sql
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/")
