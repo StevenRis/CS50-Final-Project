12ca32a9da000d2720c8a064321233ea5a1b8ae2
Stanisław Griszko <s.griszko@gmail.com>
2022-11-21 15:18:03 +0100

    fix: comment code of string replace in js, change color of footer text

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,32 +1,36 @@
 // "use strict"
 
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+    showSpinner();
+})
+
 //preloader
-function preloader(){
+function showSpinner(){
     document.querySelector("#loading").style.display = "none";
     document.querySelector("#content").style.display = "block";
     document.querySelector("footer").style.display = "block";
-
 }
 
-window.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-    preloader();
-})
 // Get all a tags with class .car-setup
 // It returns node list of a tags
-const setup_links = document.querySelectorAll('.car-setup');
+// const setup_links = document.querySelectorAll('form');
+// console.log(setup_links);
 
-// loop through a tags
-function changeTag() {
-    for (let i = 0; i < setup_links.length; i++) {
-        // Get a tag href value and slice it
-        // 21 symbols from the beginnning and 4 symbols from the end
-        const new_url = setup_links[i].href.slice(21, -4);
-        // Make change on the page
-        setup_links[i].href = new_url;
-    }
-}
+// // loop through a tags
+// function changeTag() {
+//     for (let i = 0; i < setup_links.length; i++) {
+//         // Get a tag href value and slice it
+//         // 21 symbols from the beginnning and 4 symbols from the end
+//         const new_url = setup_links[i].action.slice(26).split(' ').join('_');
+//         // const new_url = setup_links[i].action.replace('', '1');
+//         console.log(new_url);
+//         // Make change on the page
+//         setup_links[i].action = new_url;
+//     }
+// }
 
+// changeTag()
 // Get the button
 let goTopButton = document.querySelector(".goTopButton");
 
